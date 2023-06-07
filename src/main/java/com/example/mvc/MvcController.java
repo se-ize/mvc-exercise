44702994d38078c7e2e4fabba47238735179aad5
se-ize <seizenim@gmail.com>
2023-06-07 15:25:34 +0900

    Feat: add each

--- src/main/java/com/example/mvc/MvcController.java
+++ src/main/java/com/example/mvc/MvcController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class MvcController {
     @RequestMapping("/")
@@ -24,5 +28,24 @@ public class MvcController {
     public String isLoggedIn(Model model) {
         model.addAttribute("isLoggedIn", true);
         return "if-unless";
+    }
+
+    @RequestMapping("/each")
+    public String items(Model model) {
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.add("foo");
+        listOfStrings.add("bar");
+        listOfStrings.add("baz");
+
+        model.addAttribute("listOfStrings", listOfStrings);
+//        for (String item : listOfStrings);
+
+        List<Student> studentList = Arrays.asList(
+                new Student("Alex", "alex@gmail.com"),
+                new Student("Brad", "brad@gmail.com"),
+                new Student("Chad", "chad@gmail.com")
+        );
+        model.addAttribute("studentList", studentList);
+        return "each";
     }
 }
